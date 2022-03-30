6def147be66f4875794a7d891eb327a8bf484aab
Inertia123 <you@example.com>
2022-03-30 23:20:01 +0530

    completed

---
+++ script.js
@@ -0,0 +1,15 @@
+const adId = document.querySelector('.advice-id');
+const quote = document.querySelector('.quote');
+const btn = document.querySelector('.dice');
+
+function onBtnClick (){
+    fetch('https://api.adviceslip.com/advice', {cache: 'no-store'})
+        .then(resp => resp.json())
+        .then(jsonData => {
+            adId.innerText = 'advice #' + jsonData['slip']['id'];
+            quote.innerText = jsonData['slip']['advice'];
+        })
+
+}
+
+btn.addEventListener('click', onBtnClick);
